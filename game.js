1a10e3a71f6360533f6167b10cb652a5678d2362
je-martini <jesusmartini2017@gmail.com>
2022-11-21 22:21:32 -0500

    starting the icons display and the logic

--- game.js
+++ game.js
@@ -1,11 +1,35 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', start_game)
+window.addEventListener('load', set_canvas_sizes);
+window.addEventListener('resize', set_canvas_sizes);
+
+let canvas_sizes;
+let elements_sizes;
+
+function set_canvas_sizes(){
+    if (window.innerHeight > window.innerWidth){
+        canvas_sizes = window.innerWidth * 0.8
+    } else {
+        canvas_sizes = window.innerHeight * 0.8
+    }
+    
+    canvas.setAttribute('width', canvas_sizes);
+    canvas.setAttribute('height', canvas_sizes);
+    
+    elements_sizes = canvas_sizes / 10;   
+    console.log(canvas_sizes, elements_sizes);
+    start_game();
+}
 
 function start_game() {
-    game.fillRect(0, 0, 100, 100);
-    game.clearRect(0,0,50,50);
+
+    game.font = elements_sizes + 'px Verdana';          
+    game.textAling = 'end'
+
+    for (let i = 0; i < 10; i++) {    
+        game.fillText(emojis['X'], -3 + (i * elements_sizes), elements_sizes); 
+    }
 }
 
 
